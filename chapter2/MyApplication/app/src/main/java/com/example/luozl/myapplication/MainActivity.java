e3d3595c396d588115a696b20fc54610536a9d7a
luozuolin <727265606@qq.com>
2017-11-07 17:45:40 +0800

    完成各种基本组件

--- chapter2/MyApplication/app/src/main/java/com/example/luozl/myapplication/MainActivity.java
+++ chapter2/MyApplication/app/src/main/java/com/example/luozl/myapplication/MainActivity.java
@@ -2,22 +2,42 @@ package com.example.luozl.myapplication;
 
 import android.app.Activity;
 import android.app.Notification;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.textservice.TextServicesManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
+import android.widget.ViewSwitcher;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.SimpleTimeZone;
 import java.util.StringTokenizer;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -26,7 +46,7 @@ import java.util.TimerTask;
  * Created by luozl on 2017/11/6.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     int[] images=new int[]{
             R.drawable.bg_top,
             R.drawable.error,
@@ -38,7 +58,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_main);
+       //setContentView(R.layout.activity_main);
         //activity_images()
         //drawCircle();
        // setContentView(R.layout.table);
@@ -47,7 +67,121 @@ public class MainActivity extends Activity {
        // edittext();
      //   button();
        // checkbutton();
-        listview();
+        //listview();
+       // arrayadapter();
+       //imageswitcher();
+        //textswitcher();
+       viewflipper();
+    }
+    private ViewFlipper viewFlipper;
+    public void viewflipper()
+    {
+        setContentView(R.layout.viewflipper);
+        viewFlipper=(ViewFlipper)findViewById(R.id.viewflipper);
+    }
+    public void   prev1(View source)
+    {
+        viewFlipper.setInAnimation(this,R.anim.slide_in_right);
+        viewFlipper.setOutAnimation(this,R.anim.slide_out_left);
+        viewFlipper.showPrevious();
+        viewFlipper.stopFlipping();
+    }
+    public void   next1(View source)
+    {
+        viewFlipper.setInAnimation(this,R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this,R.anim.slide_out_right);
+        viewFlipper.showNext();
+        viewFlipper.stopFlipping();
+    }
+    public void   auto(View source)
+    {
+        viewFlipper.setInAnimation(this,R.anim.slide_out_left);
+        viewFlipper.setOutAnimation(this,R.anim.slide_in_right);
+        viewFlipper.startFlipping();
+    }
+     TextSwitcher textSwitcher;
+    String[] strs=new String[]
+            {
+                    "疯狂Java讲义",
+                    "疯狂Android讲义",
+                    "疯狂XML讲义",
+                    "疯狂Spring讲义"
+            };
+    public   void textswitcher()
+    {
+        setContentView(R.layout.textswitcher);
+        textSwitcher=(TextSwitcher)findViewById(R.id.textswitcher);
+        textSwitcher.setFactory(new ViewSwitcher.ViewFactory(){
+            @Override
+            public View makeView() {
+                TextView textView=new TextView(MainActivity.this);
+                textView.setTextSize(40);
+                textView.setTextColor(Color.MAGENTA);
+                return textView;
+            }
+        });
+      next(null);
+    }
+    int curStr=0;
+    public   void   next(View source)
+    {
+        textSwitcher.setText(strs[curStr++ % strs.length]);
+    }
+    public   void imageswitcher()
+    {
+        setContentView(R.layout.imageswitcher);
+        final int[] imageids=new int[]
+                {
+                        R.drawable.scrubber_disabled,R.drawable.scrubber_focussed,
+                        R.drawable.scrubber_normal,R.drawable.scrubber_pressed
+                };
+        final ImageSwitcher switcher=(ImageSwitcher)findViewById(R.id.switcher);
+        List<Map<String,Object>> list=new ArrayList<Map<String, Object>>();
+        for (int i=0;i<imageids.length;i++)
+        {
+            Map<String,Object> listitem=new HashMap<String,Object>();
+            listitem.put("image",imageids[i]);
+            list.add(listitem);
+        }
+        switcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView=new ImageView(MainActivity.this);
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                return  imageView;
+            }
+        });
+        final SimpleAdapter simpleAdapter=new SimpleAdapter(this,list,R.layout.cell,new String[]{"image"},new int[]{R.id.image1});
+        GridView gridView=(GridView)findViewById(R.id.grid01);
+        gridView.setAdapter(simpleAdapter);
+        gridView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switcher.setImageResource(imageids[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switcher.setImageResource(imageids[position]);
+            }
+        });
+    }
+    public   void arrayadapter()
+    {
+        setContentView(R.layout.arrayadapter);
+        ListView list1=(ListView)findViewById(R.id.list1);
+        ListView list2=(ListView)findViewById(R.id.list2);
+        String[] arr1=new String[]{"孙悟空","猪八戒","沙悟净"};
+        String[] arr2=new String[]{"Java","XML","Android","Spring"};
+        ArrayAdapter<String> adapter1=new ArrayAdapter<String>(this,R.layout.array_item,arr1);
+        list1.setAdapter(adapter1);
     }
     public   void listview()
     {
