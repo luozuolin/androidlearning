9bfd400e06780db80e4047fdfb9eebc8d4e152d0
luozuolin <727265606@qq.com>
2017-12-06 17:03:03 +0800

    第五章初始化

---
+++ chapter4/MyApplication/app/src/main/java/com/example/luozl/myapplication/ActivityForResult.java
@@ -0,0 +1,10 @@
+package com.example.luozl.myapplication;
+
+import android.app.Activity;
+
+/**
+ * Created by luozl on 2017/11/15.
+ */
+
+public class ActivityForResult extends Activity {
+}
---
+++ chapter4/MyApplication/app/src/main/java/com/example/luozl/myapplication/Lifecycle.java
@@ -0,0 +1,8 @@
+package com.example.luozl.myapplication;
+
+/**
+ * Created by luozl on 2017/11/15.
+ */
+
+public class Lifecycle {
+}
