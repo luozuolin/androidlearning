dcbdb0d2bc5726439971a2a3f66a0f2482c8f9af
luozuolin <727265606@qq.com>
2017-12-11 18:35:55 +0800

    第八章学习：sdfile

---
+++ chapter8/MyApplication/app/src/main/java/com/example/luozl/myapplication/SDFileActivity.java
@@ -0,0 +1,22 @@
+package com.example.luozl.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by luozl on 2017/12/11.
+ */
+
+public class SDFileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sdfile();
+    }
+    public   void sdfile()
+    {
+        setContentView(R.layout.sdfile);
+
+    }
+}
