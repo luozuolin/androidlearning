13de7951a26adf22fe5cc571609d03918167d86f
luozuolin <727265606@qq.com>
2017-12-18 16:14:19 +0800

    完成第九章学习

--- chapter9/MyApplication2/app/src/main/java/com/example/luozl/myapplication/contentobserver.java
+++ chapter9/MyApplication2/app/src/main/java/com/example/luozl/myapplication/contentobserver.java
@@ -1,11 +1,15 @@
 package com.example.luozl.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by luozl on 2017/12/15.
@@ -17,6 +21,18 @@ public class contentobserver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contentobservice);
+        if (Build.VERSION.SDK_INT >= 23) {
+            int REQUEST_CODE_CONTACT = 101;
+            String[] permissions = {Manifest.permission.READ_SMS};
+            //验证是否许可权限
+            for (String str : permissions) {
+                if (this.checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
+                    //申请权限
+                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
+                    return;
+                }
+            }
+        }
         getContentResolver().registerContentObserver(Uri.parse("content://sms"),true,
                 new SmsObserver(new Handler()));
     }
@@ -42,7 +58,8 @@ public class contentobserver extends AppCompatActivity {
                 sb.append(";subject=").append(cursor.getString(cursor.getColumnIndex("subject")));
                 sb.append(";body=").append(cursor.getString(cursor.getColumnIndex("body")));
                 sb.append(";time=").append(cursor.getLong(cursor.getColumnIndex("time")));
-                System.out.println(sb.toString());
+                Toast.makeText(contentobserver.this,sb.toString(),Toast.LENGTH_LONG).show();
+                System.out.println("发送短息:"+sb.toString());
             }
         }
     }
